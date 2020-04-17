3005280304611b762a061f2f7117c5b01d978dc2
sergiobarbieri <barbieri@itconsulting.com.br>
2020-04-16 21:18:10 -0300

    CrossOrigin

--- src/main/java/br/com/drummond/aula/resources/CRUDAlunosResources.java
+++ src/main/java/br/com/drummond/aula/resources/CRUDAlunosResources.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import br.com.drummond.aula.domain.Aluno;
 import br.com.drummond.aula.repository.AlunoRepository;
 
 @RestController
+@CrossOrigin
 public class CRUDAlunosResources {
 	
 	@Autowired
